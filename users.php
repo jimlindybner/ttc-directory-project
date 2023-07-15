5900393255c8dc4c4a3b537a3559dde0162a210d
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-07-15 16:24:29 -0400

    Update users.php

--- users.php
+++ users.php
@@ -38,23 +38,23 @@ $result = mysqli_query( $connect, $query );
     <th align="center">ID</th>
     <th align="left">Name</th>
     <th align="left">Email</th>
-    <th></th>
-    <th></th>
     <th>Active</th>
+    <th></th>
+    <th></th>
   </tr>
   <?php while( $record = mysqli_fetch_assoc( $result ) ): ?>
     <tr>
       <td align="center"><?php echo $record['user_id']; ?></td>
       <td align="left"><?php echo htmlentities( $record['first'] ); ?> <?php echo htmlentities( $record['last'] ); ?></td>
       <td align="left"><a href="mailto:<?php echo htmlentities( $record['email'] ); ?>"><?php echo htmlentities( $record['email'] ); ?></a></td>
+      <td align="center">
+        <?php echo $record['active']; ?>
+      </td>
       <td align="center"><a href="users_edit.php?user_id=<?php echo $record['user_id']; ?>">Edit</a></td>
       <td align="center">
         <?php if( $_SESSION['user_id'] != $record['user_id'] ): ?>
           <a href="users.php?delete=<?php echo $record['user_id']; ?>" onclick="javascript:confirm('Are you sure you want to delete this user?');">Delete</a>
         <?php endif; ?>
-      </td>
-      <td align="center">
-        <?php echo $record['active']; ?>
       </td>
     </tr>
   <?php endwhile; ?>
